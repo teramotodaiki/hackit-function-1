9fe4d94b4b510a50891da008f8555d546bd69341
teramotodaiki <i101323@gmail.com>
2016-09-23 12:42:48 +0900

    Update script loading

---
+++ canvas4/main.js
@@ -0,0 +1,10 @@
+// 4. 自由のキャンバス
+var p5 = require('https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.3/p5.min.js');
+new p5();
+pixelDensity(1);
+resizeCanvas(1000, 1000);
+Hack.view = canvas;
+
+background(255);
+
+// 残りの時間で、このキャンバスに自由に絵を描こう！
